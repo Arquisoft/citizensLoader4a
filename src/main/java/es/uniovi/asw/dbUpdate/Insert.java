ac05c4d9471d8e659314ff91ea6ccc3f95020516
ana bravo mediavilla <ana bravo mediavilla>
2017-02-03 20:20:42 +0100

    lectura Excel

--- src/main/java/es/uniovi/asw/dbUpdate/Insert.java
+++ src/main/java/es/uniovi/asw/dbUpdate/Insert.java
@@ -4,4 +4,5 @@ package es.uniovi.asw.dbUpdate;
  * Recibe un objeto con la información para insertar en la base de datos.
  */
 public interface Insert {
+	
 }
--- src/main/java/es/uniovi/asw/parser/RList.java
+++ src/main/java/es/uniovi/asw/parser/RList.java
@@ -1,7 +1,51 @@
 package es.uniovi.asw.parser;
 
+import java.io.File;
+import java.util.Iterator;
+import java.util.List;
+
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import es.uniovi.asw.model.Citizen;
 /**
  * Crea los subcomponentes del parser necesarios para procesar el fichero de entrada.
  */
-public class RList {
+public class RList implements ReadList{
+
+	@Override
+	public List<Citizen> read(String path) {
+		List<Citizen> ciudadanos = readFile(path);
+		return ciudadanos;
+	}
+
+	private List<Citizen> readFile(String path) {
+		XSSFWorkbook wb;
+		XSSFSheet sheet;
+		Iterator<Row> rows;
+		Row row = null;
+		Citizen ciudadano;
+		
+		try {
+			wb = new XSSFWorkbook(new File(path));
+			sheet = wb.getSheetAt(0);
+			rows = sheet.iterator();
+			
+			rows.next();
+			
+			while (rows.hasNext()) {
+				//Luego lo hago
+			}
+								
+		} catch (InvalidFormatException e) {
+			System.out.println("El fichero no es un .xlsx");
+		} catch (Exception e) {
+			String[] fileName = path.split("/");
+			System.out.println("El fichero " + fileName[fileName.length - 1] + " no existe");
+		}
+		
+		return null;
+	}
 }
