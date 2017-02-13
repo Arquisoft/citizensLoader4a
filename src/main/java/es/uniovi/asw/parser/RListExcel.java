5dae5b416340e60e73bd22969df59fbbbab372b5
Ana Bravo Mediavilla <anabravo25@hotmail.com>
2017-02-13 13:00:47 +0100

    Update RListExcel.java

--- src/main/java/es/uniovi/asw/parser/RListExcel.java
+++ src/main/java/es/uniovi/asw/parser/RListExcel.java
@@ -34,7 +34,7 @@ public class RListExcel extends RList  implements ReadList{
             String nif;
 
             boolean nameResult, surnameResult, emailResult, birthResult, addressResult, nationalityResult, nifResult;
-     //       int colNombre, colApellido,colEmail, colBirth, colAddress, colNacionalidad, colNif;
+            int colNombre = 0, colApellido = 0,colEmail = 0, colBirth = 0, colAddress = 0, colNacionalidad = 0, colNif = 0;
             StringBuilder logger = new StringBuilder();
 
             try {
@@ -43,43 +43,49 @@ public class RListExcel extends RList  implements ReadList{
                 rowIterator = sheet.iterator();
                 int columna=0;
                 row=rowIterator.next();
-//                while (columna<7){
-//                	if(row.getCell(columna).getStringCellValue().startsWith("No"))
-//                		colNombre=columna;
-//                	columna++;
-//                }
+                while (columna<7){
+                	String celda=row.getCell(columna).getStringCellValue();
+                	if(celda.startsWith("No"))
+                		colNombre=columna;
+                	else if(celda.startsWith("Ap"))
+                		colApellido=columna;
+                	else if(celda.startsWith("Cor")||celda.startsWith("Em"))
+                		colEmail=columna;
+                	else if(celda.startsWith("Fec"))
+                		colBirth=columna;
+                	else if(celda.startsWith("Dir"))
+                		colAddress=columna;
+                	else if(celda.startsWith("Nac"))
+                		colNacionalidad=columna;
+                	else if(celda.startsWith("DNI")||celda.startsWith("NIF"))
+                		colNif=columna;
+                	columna++;
+                }
 
                 int actualrow = 0;
                 while (rowIterator.hasNext()) {
                     actualrow++;
                     row = rowIterator.next();
-                    name = row.getCell(0) != null ? row.getCell(0).getStringCellValue() : null;
+                    name = row.getCell(colNombre) != null ? row.getCell(colNombre).getStringCellValue() : null;
                     nameResult = Comprobador.esTodoTexto(name);
-                    surname = row.getCell(1) != null ? row.getCell(1).getStringCellValue() : null;
+                    surname = row.getCell(colApellido) != null ? row.getCell(colApellido).getStringCellValue() : null;
                     surnameResult = Comprobador.esTodoTexto(surname);
-                    email = row.getCell(2) != null ? row.getCell(2).getStringCellValue() : null;
+                    email = row.getCell(colEmail) != null ? row.getCell(colEmail).getStringCellValue() : null;
                     emailResult = Comprobador.esEmailCorrecto(email);
                     // birthResult =
                     // Comprobador.fechaCorrecta(row.getCell(3).getStringCellValue());
                     // // Orden
                     // importante
                     birthResult = true;
-                    birth = row.getCell(3) != null && birthResult ? row.getCell(3).getDateCellValue() : null;
-                    address = row.getCell(4) != null ? row.getCell(4).getStringCellValue() : null;
+                    birth = row.getCell(colBirth) != null && birthResult ? row.getCell(colBirth).getDateCellValue() : null;
+                    address = row.getCell(colAddress) != null ? row.getCell(colAddress).getStringCellValue() : null;
                     addressResult = Comprobador.esAddressCorrecto(address);
                     addressResult = true;
-                    nationality = row.getCell(5) != null ? row.getCell(5).getStringCellValue() : null;
+                    nationality = row.getCell(colNacionalidad) != null ? row.getCell(colNacionalidad).getStringCellValue() : null;
                     nationalityResult = Comprobador.esTodoTexto(nationality);
-                    nif = row.getCell(6) != null ? row.getCell(6).getStringCellValue() : null;
+                    nif = row.getCell(colNif) != null ? row.getCell(colNif).getStringCellValue() : null;
                     nifResult = nif != null ? true : false;
 
-//                    // Se carga mas info al logger.
-//                    boolean todoOK = GenerateLogText.completeTextForLog(logger, nameResult, surnameResult, emailResult,
-//                            birthResult, addressResult, nationalityResult, nifResult, actualrow);
-
-//                    if (todoOK) {
-//                        ciudadanos.add(anadirCitizen(name, surname, email, birth, address, nationality, nif));
-//                    }
                   ciudadanos.add(anadirCitizen(name, surname, email, birth, address, nationality, nif));
 
                 }
