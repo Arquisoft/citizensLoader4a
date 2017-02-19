e5273f62c0c904bbc263f1edd0d754979779e7d9
anabravo25 <anabravo25@hotmail.com>
2017-02-19 23:12:42 +0100

    Subir codecov

--- src/main/java/es/uniovi/asw/parser/adapter/MyParserAdapter.java
+++ src/main/java/es/uniovi/asw/parser/adapter/MyParserAdapter.java
@@ -3,23 +3,25 @@ package es.uniovi.asw.parser.adapter;
 import es.uniovi.asw.parser.differentParsers.MyParser;
 import es.uniovi.asw.util.Console;
 
-public class MyParserAdapter implements GeneralParser{
+public class MyParserAdapter implements GeneralParser {
 	MyParser parser;
 	String[] args;
-	
+
 	public MyParserAdapter() {
-		
+
 	}
+
 	@Override
 	public void parse(String... args) {
 		/*
-		 * Como utiliza los mismos comandos que el parser original, no hacen falta converisones. De momento
+		 * Como utiliza los mismos comandos que el parser original, no hacen
+		 * falta converisones. De momento
 		 */
-		for(Object s : args){
-			Console.println(s.toString()+ " ");
+		for (Object s : args) {
+			Console.println(s.toString() + " ");
 		}
 		parser = new MyParser(args);
-		parser.parse();		
+		parser.parse();
 	}
 
 }
